db6c7981571777f8e566f05cf5073e41534485aa
Jens Krumsieck <info@codequake.de>
2012-12-18 21:22:29 +0100

    StartPage-Class added

---
+++ lib/page/StartPage.class.php
@@ -0,0 +1,30 @@
+﻿<?php
+/**
+** @author Jens Krumsieck
+** @package de.codequake.page.start
+** @license codeQuake-Startpage License (link coming soon)
+**/
+
+//wcf imports
+require_once(WCF_DIR.'lib/page/AbstractPage.class.php');
+
+
+class StartPage extends AbstractPage{
+    public $templateName = 'startPage';
+    
+    /**
+    ** @see: Page::readData();
+    **/
+    public function readData(){
+        parent::readData();
+        }
+    
+    /**
+    ** @see: Page::assignVariables();
+    **/
+    public function assignVariables(){
+        parent::assignVariables();
+        WCF::getTPL()->assign(array());
+    }
+}
+?>
