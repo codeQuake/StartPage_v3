4719cc39aa02f02dce61945f9043622ab935cb1a
Jens Krumsieck <info@codequake.de>
2013-01-06 13:59:01 +0100

    Happy new Year. Guys :)

---
+++ files/lib/acp/page/StartPageBoxListPage.class.php
@@ -0,0 +1,34 @@
+﻿<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/AbstractPage.class.php');
+
+
+class StartPageBoxListPage extends AbstractPage{
+    public $templateName = "startPageBoxList";
+    
+    //List of Boxes
+    public $startPageBoxes = array();
+    
+    public function readData(){
+        parent::readData();
+        
+        $sql = "SELECT * FROM wbb".WBB_N."_startpageboxes ORDER BY boxID ASC";
+        $result = WCF::getDB()->sendQuery($sql);
+        while($row = WCF::getDB()->fetchArray($result)){
+            $this->startPageBoxes[] = $row;
+        }
+        
+    }
+    
+    public function assignVariables(){
+        parent::assignVariables();
+        
+        WCFACP::getMenu()->setActiveMenuItem('TODO!');
+        
+        WCF::getTPL()->assign(array(
+                                     'boxes' =>$this->startPageBoxes));
+    }
+    
+}
+
+?>
