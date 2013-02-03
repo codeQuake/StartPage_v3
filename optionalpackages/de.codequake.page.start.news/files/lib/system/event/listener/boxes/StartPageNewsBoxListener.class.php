0071bae1ca0e4ea95d9a7d0a1e120c1dd8f434f7
Jens Krumsieck <info@codequake.de>
2013-02-03 20:17:06 +0100

    news - first run

---
+++ optionalpackages/de.codequake.page.start.news/files/lib/system/event/listener/boxes/StartPageNewsBoxListener.class.php
@@ -0,0 +1,46 @@
+<?php
+//wcf imports
+require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
+require_once(WCF_DIR.'lib/data/message/bbcode/MessageParser.class.php');
+//wbb imports
+require_once(WBB_DIR.'lib/data/thread/ThreadList.class.php');
+
+class StartPageNewsBoxListener implements EventListener{
+    public $news;
+
+    public function getNews(){
+        //get BoardIDs User can visit
+        $boardIDs = Board::getAccessibleBoardIDArray();
+        if(empty($boardIDs)){
+            $boardIDs = array(0);
+        }
+        //Check if user can visit news boards
+        $newsBoards = preg_split('#,#', STARTPAGE_NEWS_BOARDS);        
+        $boardIDs = array_intersect($boardIDs, $newsBoards);
+        $boardIDs = implode(',', $boardIDs);
+        
+        //read Threads
+        $this->news = new ThreadList();
+        if(!empty($boardIDs)){
+            $this->news->sqlConditions .= "boardID IN(".$boardIDs.")";
+            $this->news->sqlSelects .= " firstpost.message, firstpost.enableSmilies, firstpost.enableHtml, firstpost.enableBBCodes, ";
+            $this->news->sqlJoins .= "LEFT JOIN wbb".WBB_N."_post firstpost ON (firstpost.postID = thread.firstPostID)";
+            $this->news->sqlOrderBy = 'thread.time DESC';
+            $this->news->limit = STARTPAGE_NEWS_LIMIT;
+            $this->news->readThreads();
+            return $this->news->threads;
+        }
+        else return;
+    }
+    
+    public function execute($eventObj, $className, $eventName) {
+        
+        $parser = MessageParser::getInstance();
+        $parser->setOutputType('text/html');
+        $this->news = $this->getNewsList();
+        
+        //get vars into tpl ;)
+        WCF::getTPL()->assign(array('news' => $news,
+                                    'parser' => $parser));
+}
+?>
---
+++ optionalpackages/de.codequake.page.start.news/templates/news.tpl
@@ -0,0 +1,27 @@
+﻿<div class="newsBox">
+    <h3 class="subHeadline">
+        <img src="{icon}newsM.png{/icon}" alt="" /> {lang}wbb.start.news.title{/lang}
+    </h3>
+
+    {if $this->getStyle()->getVariable('messages.color.cycle')}
+        {cycle name=messageCycle values='2,1' print=false}
+    {else}
+        {cycle name=messageCycle values='1' print=false}
+    {/if}
+
+    {foreach from=$news item=entry}
+        <div class="message threadNo-{entry.threadID}">
+            <div class="messageHeader">
+                <div class="containerIcon">
+                    <img src="{icon}newsM.png{/icon}" alt="" />
+                </div>
+                <div class="containerContent">
+                    <a href="index.php?page=User&amp;userID={$entry.userID}">{$entry.username}</a> ({@$entry->time|time})
+                </div>
+            </div>
+            <div class="messageBody">
+                {@$parser->parse($entry->message, $entry->enableSmilies, $entry->enableHtml, $entry->enableBBCodes)}
+            </div>
+        </div>
+    {/foreach}
+</div>
