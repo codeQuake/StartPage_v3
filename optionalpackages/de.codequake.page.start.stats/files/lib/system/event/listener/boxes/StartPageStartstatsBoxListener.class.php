29c26c49ca1a15068c493b97dd9254434178dc01
Jens Krumsieck <info@codequake.de>
2013-02-04 21:38:27 +0100

    stats-box: initial commit

---
+++ optionalpackages/de.codequake.page.start.stats/files/lib/system/event/listener/boxes/StartPageStartstatsBoxListener.class.php
@@ -0,0 +1,20 @@
+﻿<?php
+//wcf imports
+require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
+
+class StartPageStartstatsBoxListener implements EventListener{
+
+    /**
+     ** @see IndexPage::renderStats()
+     **/
+    protected function renderStats() {
+    $stats = WCF::getCache()->get('stat');
+    WCF::getTPL()->assign('stats', $stats);
+    }
+    
+    public function execute($eventObj, $className, $eventName) {
+        $this->renderStats();
+    }
+}
+
+?>
---
+++ optionalpackages/de.codequake.page.start.stats/templates/startstats.tpl
@@ -0,0 +1,11 @@
+﻿
+<div class="border">
+      <div class="containerHead">
+            {lang}wbb.start.stats.title{/lang}
+        
+    </div>
+    <ul class="dataList">
+        {lang}wbb.start.stats.detailed{/lang}
+    </ul>
+</div>
+
